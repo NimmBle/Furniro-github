b52297fb84b8d0a772f3ee375c68c00185bca4da
Hristo Panev <hristopanev20@gmail.com>
2024-09-06 10:33:51 +0300

    add server folder

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './main/layout/header/header.component';
+import { FooterComponent } from './main/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
